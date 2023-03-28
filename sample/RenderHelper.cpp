568b2cbfb82d58e5497b3bf756c73cbe6a8fb9e3
wang.shaohui <wang.shaohui@navercorp.com>
2023-03-28 18:45:19 +0800

    add shader for UAV

--- sample/RenderHelper.cpp
+++ sample/RenderHelper.cpp
@@ -39,6 +39,18 @@ void InitShader()
 
 	{
 		shaderInfo.vsFile = dir + L"default-vs.cso";
+		shaderInfo.psFile = dir + L"output-ps.cso";
+		shaderInfo.vsBufferSize = sizeof(matrixWVP);
+		shaderInfo.psBufferSize = 0;
+		shaderInfo.vertexCount = TEXTURE_VERTEX_COUNT;
+		shaderInfo.perVertexSize = sizeof(TextureVertexDesc);
+		shader_handle shader = pGraphic->CreateShader(shaderInfo);
+		assert(shader);
+		shaders[VIDEO_SHADER_TYPE::SHADER_TEXTURE_OUTPUT] = shader;
+	}
+
+	{
+		shaderInfo.vsFile = dir + L"default-vs.cso";
 		shaderInfo.psFile = dir + L"white-ps.cso";
 		shaderInfo.vsBufferSize = sizeof(matrixWVP);
 		shaderInfo.psBufferSize = sizeof(WhiteParam);
