297cf5a551dcf5f3e5625b08c4abc647e9420ebf
wang.shaohui <wang.shaohui@navercorp.com>
2023-07-12 14:53:13 +0800

    save demo

--- sample-msaa/HLSL/tex-ps.hlsl
+++ sample-msaa/HLSL/tex-ps.hlsl
@@ -2,12 +2,10 @@
 #define MSAA_SAMPLES 8 // TODO : 外部传入
 #endif
 
-// TODO : 外部传入
-#define TEXTURE_WIDTH 1280
-#define TEXTURE_HEIGHT 720
-
 SamplerState sampleType : register(s0);
 Texture2DMS<float4, MSAA_SAMPLES> image0 : register(t0); // only supported from ps-5.0
+
+// 感觉效果也一般
 
 struct PixelInputType {
 	float4 position : SV_POSITION;
@@ -16,15 +14,17 @@ struct PixelInputType {
 
 float4 main(PixelInputType input) : SV_TARGET
 {
-	uint2 coords = uint2(input.tex.x * TEXTURE_WIDTH, input.tex.y * TEXTURE_HEIGHT);
+	uint width = 0;
+	uint height = 0;
+	uint numberOfSamples = 1;
+	image0.GetDimensions(width, height, numberOfSamples);
+
+	uint2 coords = uint2(input.tex.x * width, input.tex.y * height);
 	float4 color = float4(0.0f, 0.0f, 0.0f, 0.0f);
 
-#if MSAA_SAMPLES <= 1
-	[unroll]
-#endif
-	for (unsigned int i = 0; i < MSAA_SAMPLES; ++i) {
+	for (unsigned int i = 0; i < numberOfSamples; ++i) {
 		color += image0.Load(coords, i);
 	}
 
-	return float4(color * rcp((float)MSAA_SAMPLES));
+	return float4(color * rcp((float)numberOfSamples));
 }
