002fa453672c9f193f0661905b41d1b467fc03d5
walker-WSH <97082645@qq.com>
2023-11-05 08:26:12 +0800

    add project to detect dx error

---
+++ sample-dxError/sample-1Dlg.h
@@ -0,0 +1,46 @@
+﻿
+// sample-1Dlg.h: 头文件
+//
+
+#pragma once
+
+#include "lib-graphic/IGraphicEngine.h"
+using namespace graphic;
+
+#define TIMER_RENDER 3000
+
+// Csample1Dlg 对话框
+class Csample1Dlg : public CDialogEx {
+	// 构造
+public:
+	Csample1Dlg(CWnd *pParent = nullptr); // 标准构造函数
+	virtual ~Csample1Dlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_SAMPLE1_DIALOG };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange *pDX); // DDX/DDV 支持
+
+	void initGraphic(HWND hWnd);
+	void uninitGraphic();
+	void render();
+
+	// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnDestroy();
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	CSliderCtrl m_sliderRotate;
+	afx_msg BOOL OnEraseBkgnd(CDC *pDC);
+};
---
+++ sample-dxError/video.cpp
@@ -0,0 +1,75 @@
+﻿#include "pch.h"
+#include "sample-1Dlg.h"
+
+class DXCallback;
+
+std::shared_ptr<DXCallback> cb = std::make_shared<DXCallback>();
+IGraphicSession *pGraphic = nullptr;
+display_handle display = nullptr;
+
+
+class DXCallback : public IGraphicCallback {
+public:
+	virtual ~DXCallback() = default;
+
+	virtual void OnGraphicError(HRESULT hr)
+	{ 
+		OutputDebugStringA(__FUNCTION__);
+	}
+
+	virtual void OnRequestRebuild() 
+	{ 
+		OutputDebugStringA(__FUNCTION__);
+	}
+
+	virtual void OnBuildSuccessed(const DXGI_ADAPTER_DESC &desc) 
+	{
+		OutputDebugStringA(__FUNCTION__);
+	}
+};
+
+void Csample1Dlg::initGraphic(HWND hWnd)
+{
+	pGraphic = graphic::CreateGraphicSession();
+	AUTO_GRAPHIC_CONTEXT(pGraphic);
+
+	pGraphic->RegisterCallback(cb);
+
+	bool bOK = pGraphic->InitializeGraphic(0);
+	assert(bOK);
+
+	display = pGraphic->CreateDisplay(hWnd);
+	assert(display);
+}
+
+void Csample1Dlg::uninitGraphic()
+{
+	{
+		AUTO_GRAPHIC_CONTEXT(pGraphic);
+		pGraphic->DestroyAllGraphicObject();
+		pGraphic->UnRegisterCallback(cb.get());
+	}
+
+	graphic::DestroyGraphicSession(pGraphic);
+}
+
+void Csample1Dlg::render()
+{
+	AUTO_GRAPHIC_CONTEXT(pGraphic);
+
+	RECT rcWindow;
+	::GetClientRect(m_hWnd, &rcWindow);
+
+	pGraphic->SetDisplaySize(display, 
+				rcWindow.right - rcWindow.left,
+				rcWindow.bottom - rcWindow.top);
+
+	if (!pGraphic->IsGraphicBuilt()) {
+		if (!pGraphic->ReBuildGraphic())
+			return;
+	}
+
+	if (pGraphic->BeginRenderWindow(display)) {
+		pGraphic->EndRender();
+	}
+}
