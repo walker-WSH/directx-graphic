df47b2baa20dcc87d831498cfa7e7361154c9c8f
wangshaohui <97082645@qq.com>
2023-12-01 20:09:56 +0800

    D3D11_BLEND_INV_SRC_ALPHA

--- lib-graphic/GraphicSessionImp.cpp
+++ lib-graphic/GraphicSessionImp.cpp
@@ -640,7 +640,7 @@ bool DX11GraphicSession::InitBlendState()
 	blendStateDescription.RenderTarget[0].SrcBlend = D3D11_BLEND_SRC_ALPHA;
 	blendStateDescription.RenderTarget[0].DestBlend = D3D11_BLEND_INV_SRC_ALPHA;
 	blendStateDescription.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ONE;
-	blendStateDescription.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ONE;
+	blendStateDescription.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_INV_SRC_ALPHA;
 	blendStateDescription.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
 	blendStateDescription.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
 	blendStateDescription.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
