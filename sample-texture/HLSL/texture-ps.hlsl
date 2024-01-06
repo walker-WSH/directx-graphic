752120acb92fc9905a3e8b6d2cae1dc9fe534136
wangshaohui <97082645@qq.com>
2024-01-06 21:40:53 +0800

    check tooth

--- sample-texture/HLSL/texture-ps.hlsl
+++ sample-texture/HLSL/texture-ps.hlsl
@@ -9,5 +9,15 @@ struct PixelInputType {
 float4 main(PixelInputType input) : SV_TARGET
 {
 	float4 textureColor = image0.Sample(sampleType, input.tex);
+	
+	// ¼ì²âÑÀ³ÝÑÕÉ«
+	if (textureColor.r > 0.35 && textureColor.g > 0.35 && textureColor.b > 0.35){
+			float t1 = abs(textureColor.r - textureColor.g);
+			float t2 = abs(textureColor.g - textureColor.b);
+			if (t1 < 0.2 && t2 < 0.16){
+				return float4(0.0, 1.0, 0, 1.0);
+			}
+	}
+
 	return textureColor;
 }
